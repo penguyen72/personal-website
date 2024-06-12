11635c575587d774c25d898e3d5f073151240fa4
Peyton Nguyen <peytonn72@gmail.com>
2024-06-11 18:24:38 -0700

    Setup Cookbook Page

---
+++ app/(routes)/cookbook/page.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from 'react'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-8 my-4">
+      <p className="text-2xl font-bold">Coming Soon!</p>
+    </div>
+  )
+}
